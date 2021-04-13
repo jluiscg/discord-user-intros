6d91af3ec2544578d1b0aed7bf2a242f6276711f
Luis Cortes <jlucorg@gmail.com>
2021-04-13 17:53:07 -0500

    First working state

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const Discord = require('discord.js');
-const config = require('../config.json');
+const config = require('./config.json');
 
 const client = new Discord.Client();
 
+//set events
 client.on('ready', () => {
     //List connected servers
     console.log('Wrestling intros bot live on:');
@@ -11,23 +12,29 @@ client.on('ready', () => {
     });
   });
 
-  client.on('voiceStateUpdate', async (oldMemberState, newMemberState) => {
-    if(oldMemberState.channel == null && newMemberState.channel != null){
-        //user connected to a voice channel (not changed)
-        if(newMemberState.member.id == config.CumbiaBotLuisID){
-            //that user is el chinito
-            voice_channel = newMemberState.channel;
-            const voice_connection = await voice_channel.join()
-            const dispatcher = voice_connection.play("bin/cumbia.mp3", {seek : 27});
-            dispatcher.on('start', () => {
-              console.log('cumbia.mp3 is now playing!');
-            });
-            dispatcher.on('finish', () => {
-              console.log('cumbia.mp3 has finished playing!');
-              voice_channel.leave();
-            });
-            dispatcher.on('error', console.error);
-        }
-    }
+client.on('voiceStateUpdate', async (oldMemberState, newMemberState) => {
+  if(oldMemberState.channel == null && newMemberState.channel != null && config.users[newMemberState.member.id]){
+      //user connected to a voice channel (not changed)
+      //get song path from config
+      var song_path = config.users[newMemberState.member.id].audio.source;
+      //get channel where the user just connected
+      voice_channel = newMemberState.channel;
+      //connect to that channel
+      voice_connection = await voice_channel.join();
+      //play corresponding sound and set a dispatcher to disconnect when it's finished
+      const dispatcher = voice_connection.play(song_path, {seek : config.users[newMemberState.member.id].audio.start});
+
+      dispatcher.on('start', () => {
+        console.log(song_path + ' is now playing!');
+      });
+
+      dispatcher.on('finish', () => {
+        console.log('Stopped playing!');
+        voice_channel.leave();
+      });
+
+      dispatcher.on('error', console.error);
+  }
 });
-client.login(config.id);
+//connect bot
+client.login(config.discord_secret_token);
